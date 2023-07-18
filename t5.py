f85c0ce92be3ac40ac7ed77cdf1487c0f36f114a
chhavi <chhavirajput466@gmail.com>
2023-07-18 18:35:58 +0530

    quiz

---
+++ t5.py
@@ -0,0 +1,21 @@
+from turtle import *
+speed('fastest')
+pensize(2)
+bgcolor("black")
+pencolor("white")
+side=6
+for i in range(side):
+    fd(200)
+    
+    for i in range(side):
+        fd(100)
+        lt(360/side)
+        
+        
+        for i in range(side):
+            fd(50)
+            lt(360/side)
+    lt(360/side)
+    
+
+mainloop()
